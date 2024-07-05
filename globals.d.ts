68a73cab26f256c2deff24b1f5b80952d2550103
Okpainmo <okpainmoandrew@gmail.com>
2024-07-05 22:45:58 +0100

    refactor: update to the globals.d.ts file

--- globals.d.ts
+++ globals.d.ts
@@ -1,14 +1,14 @@
-// import { Request } from 'express';
+import { Request } from 'express';
 
-// // Augment the Express Request type - more like extending express(with ts) globally
-// declare module 'express' {
-//   interface Request {
-//     user?: {
-//       userId: string;
-//       userEmail: string;
-//       tokenStatus: string;
-//       renewedUserAccessToken: string;
-//       renewedUserRefreshToken: string;
-//     };
-//   }
-// }
+// Augment the Express Request type - more like extending express(with ts) globally
+declare module 'express' {
+  interface Request {
+    user?: {
+      userId: string;
+      userEmail: string;
+      sessionStatus: string;
+      renewedUserAccessToken: string;
+      renewedUserRefreshToken: string;
+    };
+  }
+}
