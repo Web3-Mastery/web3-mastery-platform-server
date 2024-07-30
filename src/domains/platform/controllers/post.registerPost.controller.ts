bd66df4e0289b75c8836b5a3ab1e9c4c19e8d1f1
Okpainmo <okpainmoandrew@gmail.com>
2024-07-30 18:50:18 +0100

    chore: 'platform' domain updates

--- src/domains/platform/controllers/post.registerPost.controller.ts
+++ src/domains/platform/controllers/post.registerPost.controller.ts
@@ -1,50 +1,32 @@
 import type { Request, Response } from 'express';
-import type { DeleteResult } from 'mongodb';
 import type { PostSpecs } from '../../posts/schemas/postSchema.zod.js';
 import { findPost } from '../../posts/lib/post.findPost.service.js';
-import { deletePost } from '../lib/post.deletePost.service.js';
+import { createPost } from '../../posts/lib/post.createPost.service.js';
 import { findUser } from '../../user/lib/user.findUser.service.js';
 import { findSessionActivity } from '../lib/platform.findSessionActivity.service.js';
 import { findAndUpdateUser } from '../../user/lib/user.findAndUpdateUser.service.js';
 
-// description: deletes a new platform post/content(only article-based posts/content for now)
-// request: DELETE
-// route: '/api/v1/platform/delete-post'
+// description: creates a new platform post/content(only article-based posts/content for now)
+// request: POST
+// route: '/api/v1/platform/register-post'
 // access: Public
 
 type ResponseSpecs = {
   error?: string;
   responseMessage: string;
   response?: {
-    deletedPost: PostSpecs;
-    deleteResult: DeleteResult;
+    registeredPost: PostSpecs;
     accessToken: string;
     sessionStatus?: string;
   };
 };
 
-const deletePlatformPost = async (req: Request<{}, ResponseSpecs, PostSpecs>, res: Response<ResponseSpecs>) => {
+const registerPost = async (req: Request<{}, ResponseSpecs, PostSpecs>, res: Response<ResponseSpecs>) => {
   const { postSlug } = req.body;
 
   if (req.user) {
     try {
       const { userEmail, sessionStatus, newUserAccessToken, newUserRefreshToken, subSessionActivityId: activityId } = req.user;
-
-      const user = await findUser({ email: userEmail });
-
-      if (!user || user.isAdmin !== true) {
-        return res.status(403).json({
-          error: 'request rejected',
-          responseMessage: 'only platform administrators are allowed to perform this process'
-        });
-      }
-
-      if (!user || user.isAdmin !== true) {
-        return res.status(403).json({
-          error: 'request rejected',
-          responseMessage: 'only platform administrators are allowed to perform this process'
-        });
-      }
 
       /* No need for a similar check as below, due to too much data: Zod and Mongoose will handle that. Ensure that both the Zod and
       Mongoose Schemas are strictly verified/confirmed to block incomplete or error submissions since there is no extra check here. */
@@ -55,35 +37,37 @@ const deletePlatformPost = async (req: Request<{}, ResponseSpecs, PostSpecs>, re
       //   });
       // }
 
-      const existingPost = await findPost({ postSlug: postSlug });
+      const user = await findUser({ email: userEmail });
 
-      if (!existingPost) {
-        return res.status(400).json({
-          error: 'process empty',
-          responseMessage: `platform post with postSlug: '${postSlug} does not exist or has already been deleted`
+      if (!user || user.isAdmin !== true) {
+        return res.status(403).json({
+          error: 'request rejected',
+          responseMessage: 'only platform administrators are allowed to perform this process'
         });
       }
 
-      const deletedPost = await deletePost({ postSlug: postSlug });
+      const existingPost = await findPost({ postSlug: postSlug });
 
+      if (existingPost) {
+        return res.status(400).json({
+          error: 'duplicate-post-detected',
+          responseMessage: `request unsuccessful: a post with postSlug: '${postSlug}' already exist`
+        });
+      }
+
+      const registeredPost = await createPost({ postData: req.body });
+
+      // get current user activity
       const currentUserSubSessionActivity = await findSessionActivity({ activityId });
 
-      if (
-        currentUserSubSessionActivity &&
-        user?.sessions &&
-        user?.sessions.length > 0 &&
-        deletedPost &&
-        deletedPost.acknowledged === true &&
-        newUserAccessToken
-      ) {
-        // console.log(user.sessions);
+      if (user?.sessions && user?.sessions.length > 0 && registeredPost && newUserAccessToken && currentUserSubSessionActivity) {
         currentUserSubSessionActivity.contentActivityData = {
           contentType: 'article',
-          contentTitle: existingPost.postTitle,
-          contentId: existingPost._id && existingPost._id,
-          contentUrl: existingPost.postLink
+          contentTitle: registeredPost.postTitle,
+          contentId: registeredPost._id && registeredPost._id,
+          contentUrl: registeredPost.postLink
         };
-
+        // console.log(user.sessions);
         const currentSession = user.sessions[user.sessions.length - 1];
 
         if (currentSession) {
@@ -117,10 +101,9 @@ const deletePlatformPost = async (req: Request<{}, ResponseSpecs, PostSpecs>, re
           });
 
           return res.status(201).json({
-            responseMessage: 'post/content was un-registered/deleted successfully',
+            responseMessage: 'post registered/created successfully',
             response: {
-              deleteResult: deletedPost,
-              deletedPost: existingPost,
+              registeredPost,
               accessToken: newUserAccessToken,
               sessionStatus
             }
@@ -131,8 +114,6 @@ const deletePlatformPost = async (req: Request<{}, ResponseSpecs, PostSpecs>, re
       }
 
       return;
-
-      // }
     } catch (error) {
       if (error instanceof Error) {
         console.log(error);
@@ -157,4 +138,4 @@ const deletePlatformPost = async (req: Request<{}, ResponseSpecs, PostSpecs>, re
   return;
 };
 
-export default deletePlatformPost;
+export default registerPost;
---
+++ src/domains/platform/schemas/postCategory.schema.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+export const postCategorySchema = z.object({
+  categoryName: z.string({
+    required_error: 'categoryName is required',
+    invalid_type_error: 'categoryName must be a string'
+  }),
+  categoryId: z.string().regex(/^\d{4}$/, {
+    message: 'categoryId must be a 4 digit stringed number'
+    // required_error: 'categoryId is required',
+    // invalid_type_error: 'categoryId must be a string'
+  }),
+  categoryContentType: z.string({
+    required_error: 'categoryContentType is required',
+    invalid_type_error: 'categoryContentType must be a string'
+  })
+});
+
+export type PostCategorySpecs = z.infer<typeof postCategorySchema>;
